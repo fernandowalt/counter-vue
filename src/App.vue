808e55b628dd60826cc00d6e54004569f69b301b
Walter Gomez <waltherfgs@gmail.com>
2022-04-07 10:30:30 -0500

    pruebas2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Counter v-bind:start="10" />
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <Indecision />
 </template>
 
 <script>
 import Counter from "./components/Counter.vue";
+import Indecision from "./components/Indecision.vue";
 
 export default {
   name: "App",
   components: {
     Counter,
+    Indecision,
   },
 };
 </script>
